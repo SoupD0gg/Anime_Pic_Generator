eae98320c71de245ff4892d390cc920e8cd84f8c
SoupD0gg <121968951+SoupD0gg@users.noreply.github.com>
2023-03-17 17:35:12 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const btnEl = document.getElementById("btn");
+const animeContainerEl = document.querySelector(".img-container");
+const animeImgEl = document.querySelector(".anime-img");
+const animeNameEl = document.querySelector(".anime-name");
+
+btnEl.addEventListener("click", async function() {
+	try {
+		btnEl.disabled = true;
+		btnEl.innerText = "Loading...";
+		animeNameEl.innerText = "Updating...";
+		animeImgEl.src = "spinner.svg";
+		const response = await fetch("https://api.catboys.com/img");
+		const data = await response.json();
+		btnEl.disabled = false;
+		btnEl.innerText = "Get Anime";
+		animeContainerEl.style.display = "block";
+		animeImgEl.src = data.url;
+		animeNameEl.innerText = data.artist;
+	} catch (error) {
+		console.log(error);
+		btnEl.disabled = false;
+		btnEl.innerText = "Get Anime";
+		animeNameEl.innerText = "An error ocurred, please try again!"
+	}
+})
